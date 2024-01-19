250d78e4d76b3d5be9b3d9e09e9c421e987b2755
tejasumate118 <tejasumate118@gmail.com>
2024-01-19 09:24:34 +0530

    Roman To Integer DSA

---
+++ src/java/collectionframework/RomanToInteger.java
@@ -0,0 +1,67 @@
+package collectionframework;
+
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        String orginal;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Roman Value");
+        orginal = sc.nextLine();
+        RomanValueMap obj=new RomanValueMap();
+        int result=obj.calValue(orginal);;
+        System.out.println(result);
+
+
+
+    }
+
+    private static class RomanValueMap {
+        /*Symbol       Value
+            I             1
+            V             5
+            X             10
+            L             50
+            C             100
+            D             500
+            M             1000*/
+        int getValue(char Roman) {
+            HashMap<Character, Integer> value_map = new HashMap<Character, Integer>();
+            value_map.put('I', 1);
+            value_map.put('V', 5);
+            value_map.put('X', 10);
+            value_map.put('L', 50);
+            value_map.put('C', 100);
+            value_map.put('D', 500);
+            value_map.put('M', 1000);
+            if (value_map.containsKey(Roman)) {
+                return value_map.get(Roman);
+            } else
+                return 0;
+
+        }
+
+        int result = 0;
+
+        int calValue(String s) {
+            int i=0;
+            while(i<s.length()-1)
+            {
+                if(s.charAt(i)<s.charAt(i+1)){
+                    result-=getValue(s.charAt(i));
+                    i++;
+                }
+                else{
+                    result+=getValue(s.charAt(i));
+                    i++;
+                }}
+            result+=getValue(s.charAt(s.length()-1));
+            return result;
+        }}}
+
+
+
+
+
