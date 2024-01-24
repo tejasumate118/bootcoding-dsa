770dc243e9a38ceb8f79e95c5b787a28e094e40f
tejasumate118 <tejasumate118@gmail.com>
2024-01-24 08:21:41 +0530

    LeetCode 258: Add Digits

---
+++ src/java/leetcode/Problem_258.java
@@ -0,0 +1,43 @@
+/*
+Problem 258: Add Digits
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2
+Since 2 has only one digit, return it.
+ */
+package leetcode;
+
+import java.util.Scanner;
+
+public class Problem_258 {
+    public static void main(String[] args) {
+        System.out.println("Enter a Number to find its digit sum: ");
+        Scanner sc =new Scanner(System.in);
+        int num=sc.nextInt();
+        System.out.println("The sum of digits is: "+ addDigits(num));
+
+    }
+    public static int addDigits(int num) {
+        return recurse(num);
+    }
+    public static int recurse(int num){
+        int result=0;
+        while(num!=0){
+            result+=num%10;
+            num=num/10;
+        }
+        if(result>=10){
+            result=recurse(result);
+        }
+        return result;
+
+    }
+}
