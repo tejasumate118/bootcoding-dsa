64335373a11104843eb143def924b06aca394d7b
tejasumate118 <tejasumate118@gmail.com>
2024-01-25 12:03:19 +0530

    One Plus

---
+++ src/java/leetcode/Problem_66.java
@@ -0,0 +1,54 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class Problem_66 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.print("Original Array: ");
+        printArray(arr);
+
+
+
+        System.out.print("Array after Plus One: ");
+        printArray(plusOne(arr));
+    }
+
+    // Plus One method
+    public static int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] arr = new int[n + 1];
+        arr[0] = 1;
+        return arr;
+    }
+
+    // print array funtion
+    private static void printArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+}
