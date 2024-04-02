0ca595a06c0a600958d0572577048d4a1c81bc2b
Tejas Umate <99608206+tejasumate118@users.noreply.github.com>
2024-04-02 18:57:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (86.45%) - LeetSync

--- tree/main/src/java/leetcode/58-length-of-last-word/length-of-last-word.java
+++ tree/main/src/java/leetcode/58-length-of-last-word/length-of-last-word.java
@@ -1,8 +1,13 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         int count=0;
-        s=s.trim();
-        String[] str=s.split(" ");
-        return str[str.length-1].length();
+        //s=s.trim();
+        //String[] str=s.split(" ");
+       // return str[str.length-1].length();
+       int n=s.length();
+       int i=n-1;
+       while((i>=0&&i<n)&&s.charAt(i)==' '){i--;}
+       while((i>=0&&i<n)&&s.charAt(i)!=' '){count++;i--;};
+       return count;
     }
 }
