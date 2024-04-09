ea4f1ccd5edd16b1911e732f281f1e9ebabcf75b
Tejas Umate <99608206+tejasumate118@users.noreply.github.com>
2024-04-09 10:13:43 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (97.06%) - LeetSync

---
+++ tree/main/src/java/leetcode/2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        //O(K*N)
+        int time=0;
+        int n=tickets.length;
+        for(int i=0;i<n;i++){
+            if(i<=k){
+                if(tickets[i]<=tickets[k])time+=tickets[i];
+                else time+=tickets[k];
+            }
+            else{
+                if(i>k){
+                    if(tickets[i]<tickets[k]) time+=tickets[i];
+                    else time+= tickets[k]-1;
+                }
+            }
+        }
+
+        return time;
+    }
+}
