ad846e249ef78263d718580aa40ae064b502665f
Tejas Umate <99608206+tejasumate118@users.noreply.github.com>
2024-04-24 08:14:59 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (59.37%) - LeetSync

---
+++ tree/main/src/java/leetcode/1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    HashMap <Integer, Integer> memo =new HashMap<>();
+    public int tribonacci(int n) {
+        if(n==0)return 0;
+        if(n==1 || n==2)return 1;
+        if(memo.containsKey(n)) return memo.get(n);
+        int ans=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+        memo.put(n,ans);
+        return ans;
+    }
+}
