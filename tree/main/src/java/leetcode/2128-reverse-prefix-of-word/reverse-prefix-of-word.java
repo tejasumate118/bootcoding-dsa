64185ac87d93060c68914e3b39761ecc6c5eb5e1
Tejas Umate <99608206+tejasumate118@users.noreply.github.com>
2024-05-01 17:50:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (81.91%) - LeetSync

--- tree/main/src/java/leetcode/2128-reverse-prefix-of-word/reverse-prefix-of-word.java
+++ tree/main/src/java/leetcode/2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -1,7 +1,16 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
+        //To Character Array
         char[] arr= word.toCharArray();
-        int idx= word.indexOf(ch);
+        //Finding First Occurance of ch.
+        int idx=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==ch){
+                idx=i;
+                break;
+            }
+        }
+        //Left and Right Pointers to traverse and Swap substring.
         int left=0;
         int right=idx;
         while(left<right){
@@ -11,6 +20,7 @@ class Solution {
             left++;
             right--;
         }
+        //Return The result (here arr)
         return new String(arr);
     }
 }
