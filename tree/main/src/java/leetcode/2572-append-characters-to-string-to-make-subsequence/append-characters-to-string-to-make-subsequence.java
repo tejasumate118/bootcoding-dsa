6ebe053b23982406aede604e5c0aa3aaa713b092
Tejas Umate <99608206+tejasumate118@users.noreply.github.com>
2024-06-03 22:19:00 +0530

    Time: 4 ms (92.09%) | Memory: 44.4 MB (99.30%) - LeetSync

--- tree/main/src/java/leetcode/2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.java
+++ tree/main/src/java/leetcode/2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.java
@@ -1,12 +1,12 @@
 class Solution {
     public int appendCharacters(String s, String t) {
-    
+        int lent=t.length();
         int ptr=-1;
         for(char ch:s.toCharArray()){
-            if(ptr<t.length()-1 && (ch==t.charAt(ptr+1))){
+            if(ptr<lent-1 && (ch==t.charAt(ptr+1))){
                 ptr++;
             }
         }
-        return t.length()-ptr-1;
+        return lent-ptr-1;
     }
 }
