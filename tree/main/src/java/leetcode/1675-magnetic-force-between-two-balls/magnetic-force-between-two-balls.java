aa5c5a0ab1366105de6685b7fff645bc0acca67b
Tejas Umate <99608206+tejasumate118@users.noreply.github.com>
2024-06-20 21:32:56 +0530

    Time: 43 ms (58.58%) | Memory: 55.8 MB (52.68%) - LeetSync

---
+++ tree/main/src/java/leetcode/1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxDistance(int[] position, int m) {
+        Arrays.sort(position);
+        int l = 0; //Can also be 1 cause each ball is placed in unique buckets.
+        int r= position[position.length-1];
+        int ans = -1;
+        while(l<=r){
+            int mid= (l+r)/2;
+            if(isValidDist(mid, position, m)){
+                ans=mid;
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return ans;
+    }
+    private boolean isValidDist(int mid,int[] position, int m ){
+        int countOfBallsPlaced=1, prevId=0;
+        
+        for(int i=1; i< position.length;i++){
+            if(position[i]-position[prevId]>=mid){
+                countOfBallsPlaced++;
+                prevId=i;
+            }
+        }
+        return countOfBallsPlaced>=m;
+    }
+}
