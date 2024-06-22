6c43a1e8589d57c84f63936973c2fff734161e06
Tejas Umate <99608206+tejasumate118@users.noreply.github.com>
2024-06-22 09:08:51 +0530

    Time: 7 ms (96.52%) | Memory: 52.4 MB (98.50%) - LeetSync

---
+++ tree/main/src/java/leetcode/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int ans=0;
+        int oddCount=0;
+        int[] prefixCount=new int[nums.length+1];
+        prefixCount[0]=1;
+        for(int i=0;i<nums.length;i++){
+            int num=nums[i];
+            if(num%2!=0){
+                oddCount++;
+            }
+            
+            if(oddCount>=k){
+                ans+=prefixCount[oddCount-k];
+            }
+            prefixCount[oddCount]++;
+            
+            
+        }
+    return ans;
+        
+    }
+}
