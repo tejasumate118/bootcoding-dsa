799de95f9d1c64e8255fcbbc899b97469b4878a9
Tejas Umate <99608206+tejasumate118@users.noreply.github.com>
2024-06-23 07:45:27 +0530

    Time: 53 ms (41.43%) | Memory: 48.5 MB (65.49%) - LeetSync

--- tree/main/src/java/leetcode/2809-create-hello-world-function/create-hello-world-function.js
+++ tree/main/src/java/leetcode/2809-create-hello-world-function/create-hello-world-function.js
@@ -4,9 +4,11 @@
 var createHelloWorld = function() {
     
     return function(...args) {
-        return(`Hello World`);
+        return "Hello World"
     }
 };
+
+
 
 /**
  * const f = createHelloWorld();
