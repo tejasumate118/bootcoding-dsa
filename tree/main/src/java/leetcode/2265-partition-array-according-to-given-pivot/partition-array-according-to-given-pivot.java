43bde062b69525c281c3a187383be4588704d43d
Tejas Umate <99608206+tejasumate118@users.noreply.github.com>
2025-03-03 20:58:07 +0530

    Time: 5 ms (85.39%) | Memory: 68.4 MB (10.03%) - LeetSync

--- tree/main/src/java/leetcode/2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ tree/main/src/java/leetcode/2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,20 +1,13 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        ArrayList<Integer> f = new ArrayList<>();
-        ArrayList<Integer> p = new ArrayList<>();
-        ArrayList<Integer> l = new ArrayList<>();
-
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i]<pivot) f.add(nums[i]);
-            else if(nums[i]==pivot) p.add(nums[i]);
-            else l.add(nums[i]);
-        }
-        f.addAll(p);
-        f.addAll(l);
         int[] res = new int[nums.length];
-        for (int i = 0; i< nums.length;i++) {
-            res[i] =f.get(i);
+        int l=0;
+        int r=nums.length-1;
+        for(int i=0,j =nums.length-1;i<nums.length;i++,j--){
+            if(nums[j]>pivot) res[r--]=nums[j];
+            if(nums[i]<pivot) res[l++]=nums[i];
         }
+        while(l<=r) res[l++]=pivot;
         return res;
         
     }
