4a3bd8f91b004aeea2d06530ca36e6a49dbd987a
Tejas Umate <99608206+tejasumate118@users.noreply.github.com>
2025-03-05 21:35:21 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (59.02%) - LeetSync

--- tree/main/src/java/leetcode/2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
+++ tree/main/src/java/leetcode/2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
@@ -1,12 +1,5 @@
 class Solution {
     public long coloredCells(int n) {
-        long res=0;
-        long curr =0;
-        for(int i = 1;i<n;i++) {
-            curr+=4;
-            res+=curr;
-        }
-        return res+1;
-        
+        return 1+(long)2*n*(n-1);
     }
 }
