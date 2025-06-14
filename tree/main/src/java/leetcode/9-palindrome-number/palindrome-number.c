38edaa0c134f3b99a773c19c8ff4a88dd0064842
Tejas Umate <99608206+tejasumate118@users.noreply.github.com>
2025-06-14 11:27:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (82.61%) - LeetSync

--- tree/main/src/java/leetcode/9-palindrome-number/palindrome-number.c
+++ tree/main/src/java/leetcode/9-palindrome-number/palindrome-number.c
@@ -1,12 +1,11 @@
 bool isPalindrome(int x){
     long rem=0;
     int num=x;
-    if(num<0){return false;}
-    if(num%10 ==0 && num!=0) return false;
+    if(num<0 || num%10 ==0 && num!=0) return false;
     while(num!=0){
         rem = rem*10 + num%10;
         num=num/10;
-         }
+    }
      return rem==x;    
 
 }
