b90756c2f2b25945ba238ac5424981674908ced5
Tejas Umate <99608206+tejasumate118@users.noreply.github.com>
2025-07-04 08:55:19 +0530

    Time: 1 ms (100.00%) | Memory: 43 MB (85.37%) - LeetSync

--- tree/main/src/java/leetcode/3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.java
+++ tree/main/src/java/leetcode/3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.java
@@ -2,11 +2,14 @@ class Solution {
     public char kthCharacter(long k, int[] operations) {
         //traking back cause we know that the first character is 'a'
         int opsCount = 0;
+        //k>1 cause we know k=1 is 'a'
         while(k>1){
+            //cal. of length of the prev string from which the string consisting k was formed
             int jump = (int) Math.ceil(Math.log(k) / Math.log(2));
             long prevLength = (long)Math.pow(2, jump-1);
             //k can be equal to prevLength which means that k-prevLength will go zero thus prevent it
             if(k==prevLength) break;
+            // k-prevLength is the character from previous string from which the current k was formed
             k -=prevLength;
             // if operation[jump-1] is 0 then the character is same thus no need to perform operation
             if(operations[jump-1]==1) opsCount++;
