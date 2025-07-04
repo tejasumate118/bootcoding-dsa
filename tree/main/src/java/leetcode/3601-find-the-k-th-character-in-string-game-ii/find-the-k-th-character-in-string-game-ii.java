697edc9074983f90d2ed3b83846a8f697733a196
Tejas Umate <99608206+tejasumate118@users.noreply.github.com>
2025-07-04 08:49:59 +0530

    Time: 1 ms (100.00%) | Memory: 43.6 MB (20.73%) - LeetSync

--- tree/main/src/java/leetcode/3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.java
+++ tree/main/src/java/leetcode/3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.java
@@ -1,15 +1,18 @@
 class Solution {
     public char kthCharacter(long k, int[] operations) {
+        //traking back cause we know that the first character is 'a'
         int opsCount = 0;
-        while(k!=1){
-            if (k <= 1) break;
+        while(k>1){
             int jump = (int) Math.ceil(Math.log(k) / Math.log(2));
             long prevLength = (long)Math.pow(2, jump-1);
+            //k can be equal to prevLength which means that k-prevLength will go zero thus prevent it
+            if(k==prevLength) break;
             k -=prevLength;
-
+            // if operation[jump-1] is 0 then the character is same thus no need to perform operation
             if(operations[jump-1]==1) opsCount++;
         }
         char result = 'a';
+        //only have 26 letters in alphabet so round it up by modulo 26
         opsCount = opsCount%26;
         while(opsCount-->0){
             result = nextChar(result);
@@ -20,6 +23,7 @@ class Solution {
     }
 
     private char nextChar(char ch){
-        return (char) ('a' + (ch - 'a' + 1) % 26);
+        //nxt character calculation
+        return (char)('a'+(ch-'a'+1)%26);
     }
 }
