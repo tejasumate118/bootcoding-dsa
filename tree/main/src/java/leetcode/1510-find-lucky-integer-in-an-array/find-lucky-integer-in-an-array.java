dbeb310fc546664cccf57f1de78772910af525da
Tejas Umate <99608206+tejasumate118@users.noreply.github.com>
2025-07-05 07:53:48 +0530

    Time: 1 ms (99.94%) | Memory: 43.2 MB (84.84%) - LeetSync

--- tree/main/src/java/leetcode/1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
+++ tree/main/src/java/leetcode/1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
@@ -1,14 +1,17 @@
 class Solution {
     public int findLucky(int[] arr) {
+
+    /*
+        // First Solution
         HashMap<Integer, Integer> freq = new HashMap<>();
         for(var num : arr)
             freq.put(num, freq.getOrDefault(num,0)+1);
-        /* int largestLucky = freq.entrySet().stream()
-            .filter(e -> Objects.equals(e.getKey(),e.getValue()) )
-            .mapToInt(Map.Entry::getKey)
-            .max()
-            .orElse(-1);
-        */
+        //int largestLucky = freq.entrySet().stream()
+           // .filter(e -> Objects.equals(e.getKey(),e.getValue()) )
+           // .mapToInt(Map.Entry::getKey)
+           // .max()
+           // .orElse(-1);
+        
         int largestLucky = -1;
         for(var entry : freq.entrySet())
             if(entry.getKey() == entry.getValue()) 
@@ -16,6 +19,15 @@ class Solution {
                 //largestLucky = Math.max(largestLucky, entry.getKey());
         
         return largestLucky;
+    */
+    
+
+    int[] freq = new int[501];
+    for(int ele : arr) freq[ele]++;
+    int largestLucky = -1;
+    for(int i = 1; i<501;i++) largestLucky = (freq[i]==i)&&(largestLucky<i)?i:largestLucky;
         
+    
+    return largestLucky;   
     }
 }
