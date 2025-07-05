19c53eec714f5476acb6f97630f29e41c3d1342e
Tejas Umate <99608206+tejasumate118@users.noreply.github.com>
2025-07-05 07:40:02 +0530

    Time: 5 ms (72.10%) | Memory: 43.6 MB (23.80%) - LeetSync

--- tree/main/src/java/leetcode/1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
+++ tree/main/src/java/leetcode/1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
@@ -2,11 +2,17 @@ class Solution {
     public int findLucky(int[] arr) {
         HashMap<Integer, Integer> freq = new HashMap<>();
         for(var num : arr) freq.put(num, freq.getOrDefault(num,0)+1);
-        int largestLucky = freq.entrySet().stream()
+        /* int largestLucky = freq.entrySet().stream()
             .filter(e -> Objects.equals(e.getKey(),e.getValue()) )
             .mapToInt(Map.Entry::getKey)
             .max()
             .orElse(-1);
+        */
+        int largestLucky = -1;
+        for(var entry : freq.entrySet()){
+            if(entry.getKey() == entry.getValue()) 
+                largestLucky = largestLucky<entry.getKey()? entry.getKey():largestLucky;
+        }
         return largestLucky;
         
     }
